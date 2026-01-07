74a38ed80afbd8f1b7b7ad7de38480aa9104fdd0
chkeethi2001 <chkeethi2001@gmail.com>
2026-01-07 11:48:16 +0530

    Delete backend directory

--- backend/hash.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcryptjs";
-
-async function run() {
-  const hash = await bcrypt.hash("Admin@123", 8);
-  console.log(hash);
-}
-
-run();
--- backend/routes/leadRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { submitLead } from "../controllers/leadController.js";
-
-const router = express.Router();
-
-router.post("/contact", submitLead);
-
-export default router;
